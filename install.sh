419dc5e0e4ef31d31b10d6c0715f52887421a69f
mozhenghua <baisui@qlangtech.com>
2025-05-26 18:19:52 +0800

    upgrade flink-cdc version to v3.4.0

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #添加
 
-mvn install -Dmaven.test.skip=true -Dfast -pl flink-cdc-connect/flink-cdc-source-connectors/flink-connector-kingbase-cdc\
+mvn deploy  -Dmaven.test.skip=true  -Dfast -pl flink-cdc-connect/flink-cdc-source-connectors/flink-connector-kingbase-cdc\
 ,flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-paimon
