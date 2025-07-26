fdce05fb8fd3cfa0d4a5e51f1f025f661abed5d2
mozhenghua <baisui@qlangtech.com>
2025-07-26 17:52:04 +0800

    add profile docs-and-source

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #添加
 
-mvn clean deploy   -Dmaven.test.skip=true -Ptis -Dfast \
+mvn clean deploy   -Dmaven.test.skip=true -Ptis,docs-and-source -Dfast \
 -pl flink-cdc-connect/flink-cdc-source-connectors/flink-connector-kingbase-cdc\
 ,flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-paimon\
 ,flink-cdc-connect/flink-cdc-source-connectors/flink-connector-mysql-cdc\
